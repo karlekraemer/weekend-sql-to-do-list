9590882cb5840416231e5ba34e9dc0fc8825ef7f
Karl Kraemer <karlekraemer@gmail.com>
2023-01-03 08:48:03 -0600

    get working properly and table is appending to DOM. Need to finish done and delete buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,56 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('we ready');
+    getTasks();
+    $('#submit').on('click', postTask);
 }
 
+function postTask () {
+    console.log('submit button click');
+    let taskObject = {
+        task: $('#new-task').val(),
+        status: 'work to do'
+    };
+    $.ajax({
+        type: 'POST',
+        url: 'taskLibrary',
+        data: taskObject
+    }).then( function (response) {
+        $('#new-task').val('')
+        getTasks();
+    });
+}
+
+function getTasks() {
+    $('#incompleteTableBody').empty();
+    $.ajax({
+        type: 'GET',
+        url: '/taskLibrary'
+    }).then( function (response) {
+        console.log('GET /tasklibrary response', response);
+        for (let i = 0; i < response.length; i++) {
+            $('#incompleteTableBody').append(`
+                <tr data=id${response[i].id}>
+                    <td>${response[i].task}</td>
+                    <td>
+                        <p id="status-change-label" for="status-change">${response[i].status}</p>
+                        <button class="status-cahnge">done</button>
+                    </td>
+                    <td>
+                        <p id="delete-label" for="delete">delete task</p>
+                        <button class="delete">delete</buton>
+                    </td>
+                </tr>
+            `)
+        }
+        for(let i = 0; i < response.length; i++) {
+            if (response[i].status = "completed") {
+                console.log('change complete');
+                $('#status-change-label').parent().addClass('left-color');
+            }
+            else if (response[i].status == "work to do"){
+                console.log('no backbround change');
+            }
+        }
+    });
+}
--- server/routes/taskLibrary.js
+++ server/routes/taskLibrary.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool');
 
 
 router.get('/', (req, res) => {
-    let queryText = 'SELECT * from task_table ORDER BY "id" asc;';
+    let queryText = 'SELECT * from tasks ORDER BY "id" asc;';
     pool.query(queryText)
     .then((result) => {
         console.log('results from database', result);
@@ -13,20 +13,38 @@ router.get('/', (req, res) => {
     .catch((error) => {
         console.log('error making a query', error);
         res.sendStatus(500);
-    })
+    });
 });
 
-router.get('/', (req, res) => {
-    let queryText = 'SELECT * from task_table ORDER BY "id" asc;';
+router.get('/:id', (req, res) => {
+    console.log('in get request', req.params.id);
+    const queryText = `SELECT * from tasks WHERE id = ${req.params.id};`;
     pool.query(queryText)
     .then((result) => {
-        console.log('results from database', result);
+        console.log('results from DB', result);
         res.send(result.rows);
     })
     .catch((error) => {
-        console.log('error making a query', error);
+        console.log('error making query', error);
         res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
+    const newTask = req.body;
+    const queryText = `
+        INSERT INTO "tasks" ("task", "status")
+        VALUES ($1, $2);
+    `;
+    pool.query(queryText, [newTask.task, newTask.status])
+    .then((result) => {
+        console.log('result', result);
+        res.sendStatus(201);
     })
+    .catch((error) => {
+        console.log('error making insert query', error);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
